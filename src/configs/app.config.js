76b92492c6f2d30933a6edc3d2e231ab5a613a58
didier <didier.djakoua@gmail.com>
2025-10-08 16:53:03 +0100

    fixed avaris csv + mux ristourn1

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,8 +1,7 @@
 const appConfig = {
-    // apiPrefix: 'http://82.165.212.140:5000',
-    apiPrefix: 'http://localhost:5500',
+    apiPrefix: 'http://82.165.212.140:5000',
+    // apiPrefix: 'http://localhost:5500',
 
-    // apiPrefix: '/api',
     authenticatedUserRoles: ['ADMINISTRATOR', 'EMPLOYEE', 'CLIENT'],
     authenticatedEntryPath: '/app/dashboard',
     unAuthenticatedEntryPath: '/sign-in',
